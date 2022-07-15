63518473acbbbb809946c682b674d3e9510b1327
LendiHop <mishin.danil2017@gmail.com>
2022-07-15 10:46:44 +0200

    active beacon account check

--- index.js
+++ index.js
@@ -9,18 +9,21 @@ const initWallet = async () => {
   try {
     const dAppClient = new DAppClient({ name: "Michelson Data Sign" });
 
-    const signingMichelsonData = packDataBytes({
-      prim: 'Pair', args: [
-        { string: 'if you can read this message than task is completed :)' }, // We've hardcoded the data for simplicity
-        { int: '123456789' }
-      ]
-    }).bytes;
+    const activeAccount = await dAppClient.getActiveAccount();
+    if (activeAccount) {
+      const signingMichelsonData = packDataBytes({
+        prim: 'Pair', args: [
+          { string: 'if you can read this message than task is completed :)' },
+          { int: '123456789' }
+        ]
+      }).bytes;
 
-    const walletSignedData = await dAppClient.requestSignPayload({
-      signingType: SigningType.MICHELINE,
-      payload: signingMichelsonData
-    });
-    console.log(walletSignedData.signature);
+      const walletSignedData = await dAppClient.requestSignPayload({
+        signingType: SigningType.MICHELINE,
+        payload: signingMichelsonData
+      });
+      console.log(walletSignedData.signature);
+    }
   } catch (error) {
     console.log(error);
   }
