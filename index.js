a8dee4242abd6dc78c0bb039f03effeee022e553
LendiHop <mishin.danil2017@gmail.com>
2022-07-15 10:55:17 +0200

    active beacon account check

--- index.js
+++ index.js
@@ -9,21 +9,30 @@ const initWallet = async () => {
   try {
     const dAppClient = new DAppClient({ name: "Michelson Data Sign" });
 
+    // This code should be called every time the page is loaded or refreshed to see if the user has already connected to a wallet.
     const activeAccount = await dAppClient.getActiveAccount();
     if (activeAccount) {
-      const signingMichelsonData = packDataBytes({
-        prim: 'Pair', args: [
-          { string: 'if you can read this message than task is completed :)' },
-          { int: '123456789' }
-        ]
-      }).bytes;
-
-      const walletSignedData = await dAppClient.requestSignPayload({
-        signingType: SigningType.MICHELINE,
-        payload: signingMichelsonData
-      });
-      console.log(walletSignedData.signature);
+      // If defined, the user is connected to a wallet.
+      // You can now do an operation request, sign request, or send another permission request to switch wallet
+      console.log("Already connected:", activeAccount.address);
+    } else {
+      const permissions = await dAppClient.requestPermissions();
+      console.log("New connection:", permissions.address);
     }
+
+
+    const signingMichelsonData = packDataBytes({
+      prim: 'Pair', args: [
+        { string: 'if you can read this message than task is completed :)' }, // We've hardcoded the data for simplicity
+        { int: '123456789' }
+      ]
+    }).bytes;
+
+    const walletSignedData = await dAppClient.requestSignPayload({
+      signingType: SigningType.MICHELINE,
+      payload: signingMichelsonData
+    });
+    console.log(walletSignedData.signature);
   } catch (error) {
     console.log(error);
   }
